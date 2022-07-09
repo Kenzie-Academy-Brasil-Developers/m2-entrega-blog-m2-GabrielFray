4421972baab47aef6caa8aed62a768aca50ee144
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-07-09 13:32:14 -0300

    fix: corrigi algum bug

---
+++ src/js/Delete.post.js
@@ -0,0 +1,14 @@
+import Api from "./Api.js";
+import RenderPost from "./Render.post.js";
+
+export default class DeleteButton {
+  static deleteImg = document.querySelectorAll(".delete_img");
+  static async deletePostUser() {
+    this.deleteImg.forEach((element) => {
+      element.addEventListener("click", async () => {
+        await Api.deletePost(element.id);
+        location.reload();       
+      });
+    });
+  }
+}
